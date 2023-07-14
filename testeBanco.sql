5d39e3084c38e2fa6e79609542907d7dfac45e7f
pedrohoppe88 <pedrohenriquehoppe6@gmail.com>
2023-07-14 18:16:13 -0300

    testeBanco

--- testeBanco.sql
+++ testeBanco.sql
@@ -1,5 +1,6 @@
+-- Tabela "Users" - para armazenar informações dos usuários
 CREATE TABLE Users (
-  user_id INT PRIMARY KEY,
+  user_id INT PRIMARY KEY AUTO_INCREMENT,
   username VARCHAR(255),
   password VARCHAR(255),
   other_user_data VARCHAR(255)
@@ -7,44 +8,27 @@ CREATE TABLE Users (
 
 -- Tabela "Items" - para armazenar informações dos itens
 CREATE TABLE Items (
-  item_id INT PRIMARY KEY,
+  item_id INT PRIMARY KEY AUTO_INCREMENT,
   item_name VARCHAR(255),
   item_type VARCHAR(255),
   other_item_data VARCHAR(255)
 );
 
--- Tabela "Inventory" - para armazenar os itens no inventário dos usuários
-CREATE TABLE Inventory (
-  user_id INT,
-  item_id INT,
-  quantity INT,
-  added_by INT,
-  removed_by INT,
-  FOREIGN KEY (user_id) REFERENCES Users(user_id),
-  FOREIGN KEY (item_id) REFERENCES Items(item_id),
-  FOREIGN KEY (added_by) REFERENCES Users(user_id),
-  FOREIGN KEY (removed_by) REFERENCES Users(user_id)
-);
-
--- Tabela "Groups" - para armazenar informações dos grupos
-CREATE TABLE Groups (
-  group_id INT PRIMARY KEY,
+CREATE TABLE grupo (
+  group_id INT PRIMARY KEY AUTO_INCREMENT,
   group_name VARCHAR(255),
   other_group_data VARCHAR(255)
 );
 
--- Tabela "Group_Members" - para armazenar informações dos membros dos grupos
-CREATE TABLE Group_Members (
-  group_id INT,
-  user_id INT,
-  FOREIGN KEY (group_id) REFERENCES Groups(group_id),
-  FOREIGN KEY (user_id) REFERENCES Users(user_id)
-);
-
+-- Tabela "Group_Inventory" - para armazenar os itens no inventário dos grupos
 CREATE TABLE Group_Inventory (
   group_id INT,
   item_id INT,
   quantity INT,
-  FOREIGN KEY (group_id) REFERENCES Groups(group_id),
-  FOREIGN KEY (item_id) REFERENCES Items(item_id)
+  added_by INT,
+  removed_by INT,
+  FOREIGN KEY (group_id) REFERENCES grupo(group_id),
+  FOREIGN KEY (item_id) REFERENCES Items(item_id),
+  FOREIGN KEY (added_by) REFERENCES Users(user_id),
+  FOREIGN KEY (removed_by) REFERENCES Users(user_id)
 );
