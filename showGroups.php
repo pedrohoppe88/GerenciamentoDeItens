1ee313d81ffcf117c3438dbf99d69f1fe99add43
pedrohoppe88 <pedrohenriquehoppe6@gmail.com>
2023-12-06 21:41:39 -0300

    9g797fg

--- showGroups.php
+++ showGroups.php
@@ -170,5 +170,6 @@ $grupos = $stmt->fetchAll(PDO::FETCH_ASSOC);
     </div>
 
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
