68aa1b081f753375b98484a73b7c7618fb0d2f0e
pedrohoppe88 <pedrohenriquehoppe6@gmail.com>
2024-02-18 13:22:48 -0300

    ugpifg

--- controller/grupoController.php
+++ controller/grupoController.php
@@ -23,7 +23,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         exit();
     }
 
-    
 
     if ($grupo && password_verify($senha_grupo, $grupo['senha'])) {
         $_SESSION['grupo_id'] = $grupo['ID'];
